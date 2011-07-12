1b9169cf4c614d1b0dfd3358e8eb823dcc367038
Caius Brindescu <brind@illinois.edu>
2011-07-12 17:08:33 -0500

    Made the simple test a bit more complicated

--- subjects/racefix/Foo.java
+++ subjects/racefix/Foo.java
@@ -16,21 +16,21 @@ public class Foo {
 	
 	public static class Mumu {
 		public int x;
+		public Mumu next = null;
 		
 	}
 
 	public void simple1() {
 		Bubu b = new Bubu();
-		
 	}
 	
-	public void simple2() {
+	public void simple1Label() {
 		Bubu b = new Bubu();
 		b.m1 = new Mumu();
 		Mumu v1 = b.m1;
 	}
 	
-	public void simple3() {
+	public void simpleWithUninteresting() {
 		Mumu mumu = new Mumu();
 		
 		Bubu b = new Bubu();
@@ -38,5 +38,111 @@ public class Foo {
 		b.m2 = mumu;
 		Mumu v1 = b.m1;
 		v1.x = 10;
+	}
+	
+	public void simplePhi() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = new Bubu();
+		b.m1 = mumu;
+		b.m2 = mumu;
+		Mumu v1;
+		if (mumu.x == 21)
+			v1 = b.m1;
+		else
+			v1 = b.m2;
+		v1.x = 10;
+	}
+	
+	public void simpleCalls() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = new Bubu();
+		b.m1 = mumu;
+		b.m2 = mumu;
+		writeField(b);
+	}
+
+	private void writeField(Bubu b) {
+		Mumu v1 = b.m1;
+		v1.x = 10;
+	}
+	
+	public void simpleCalls2() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = new Bubu();
+		writeField2(mumu, b);
+	}
+
+	private void writeField2(Mumu mumu, Bubu b) {
+		b.m1 = mumu;
+		b.m2 = mumu;
+		Mumu v1 = b.m1;
+		v1.x = 10;
+	}
+	
+	public void simpleWithReturn() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = makeBubu(mumu);
+		Mumu v1 = b.m1;
+		v1.x = 10;
+	}
+
+	private Bubu makeBubu(Mumu mumu) {
+		Bubu b = new Bubu();
+		b.m1 = mumu;
+		b.m2 = mumu;
+		return b;
+	}
+	
+	//doesn't work on context insensitive
+	public void simpleCalls3() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = new Bubu();
+		b.m1 = mumu;
+		b.m2 = mumu;
+		writeField(b);
+		
+		Bubu b1 = new Bubu();
+		b1.m1 = mumu;
+		writeField(b1);
+	}
+	
+	public void simpleWithReturn2() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b1 = makeBubu(mumu);
+		Bubu b = makeBubu(mumu);
+		Mumu v1 = b.m1;
+		v1.x = 10;
+	}
+	
+	public void simpleRecursive() {
+		Mumu mumu = new Mumu();
+		
+		Bubu b = null;
+		while(mumu.x > 0) {			
+			b = new Bubu();
+			b.m1 = mumu;
+			b.m2 = mumu;
+		}
+		
+		Mumu v1 = b.m1;
+		v1.x = 10;
+	}
+	
+	public void simpleRecursive2() {
+		Mumu p = new Mumu();
+		
+		while(p.x > 0) {			
+			Mumu p1 = new Mumu();
+			p.next = p1;
+			p = p1;
+		}
+		
+		p.x = 10;
 	}
 }
